319492d5e1c7fbc3c3b40264d2e8471619551277
trentonbit <trentondevsui@gmail.com>
2024-03-27 17:11:40 -0500

    Initial script

---
+++ near/near.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if hash near 2>/dev/null; then
+    echo "near is installed"
+else
+    npm install -g near-cli
+fi
+
+export NEAR_NETWORK=mainnet
+export NEAR_MAINNET_RPC=https://near.lava.build/lava-referer-8dacf697-a98b-4b84-bc0a-6782688c9bf3/
+
+
+echo  Hi dude!
+echo  Please set your Near AccountId and your seedPhrase
+read -p 'AccountId: ' account_id
+read -p 'seedPhrase: ' seedprhase
+near add-credentials $account_id --seedPhrase "$seedprhase"
+
+echo Getting your balance in range of 20 and 60 seconds!
+random_number=$((20 + RANDOM % 60))
+
+while true
+do
+near state $account_id
+sleep $random_number
+done
