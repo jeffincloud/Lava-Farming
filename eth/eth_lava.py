15e55d1fe00d8913e6738717d3231b6f758dcdd1
trentonbit <trentondevsui@gmail.com>
2024-03-31 20:31:18 -0500

    ETH script asking for wallet input

--- eth/eth_lava.py
+++ eth/eth_lava.py
@@ -1,11 +1,13 @@
 import time
 from web3 import Web3
 
+eth_wallet = input("please add your eth wallet")
+
 def get_eth_balance(wallet_address, rpc_endpoint):
     # Connect to the Ethereum node
     web3 = Web3(Web3.HTTPProvider(rpc_endpoint))
 
-    # Check if the connection is successful
+    # Check if the connection is successful ß
     if web3.is_connected():
         # Get the balance of the wallet address
         balance_wei = web3.eth.get_balance(wallet_address)
@@ -18,8 +20,8 @@ def get_eth_balance(wallet_address, rpc_endpoint):
 # Function to repeatedly fetch and print the balance every minute
 def run():
     while True:
-        wallet_address = "YOUR ETH WALLET ADDRESS"
-        rpc_endpoint = "YOUR CUSTOM RPC ENDPOINT"
+        wallet_address = eth_wallet
+        rpc_endpoint = "https://eth1.lava.build/lava-referer-8dacf697-a98b-4b84-bc0a-6782688c9bf3/"
         balance = get_eth_balance(wallet_address, rpc_endpoint)
         if balance is not None:
             print(f"Balance of {wallet_address}: {balance} ETH")
